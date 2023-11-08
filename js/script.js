3c0a554214758ca604e8fe2793aa02dcda496802
Ayomide Olisa <lekelisa247@gmail.com>
2023-11-08 13:26:25 +0100

    aymedic project

--- js/script.js
+++ js/script.js
@@ -11,3 +11,14 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 }
 
+// function toggleMode() {
+//     const body = document.body;
+
+//     body.classList.toggle("dark-mode");
+//     const button = document.getElementById("toggleButton");
+//     if (body.classList.contains("dark-mode")) {
+//       button.innerHTML = '<i class="fas fa-toggle-on"></i>';
+//     } else {
+//       button.innerHTML = '<i class="fas fa-toggle-off"></i>';
+//     }
+//   }
